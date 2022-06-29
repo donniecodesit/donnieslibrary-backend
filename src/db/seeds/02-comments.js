0ef01472f92089b50f666931f973e981fc5af31a
Donovan Laws <91988909+donovanlaws@users.noreply.github.com>
2022-06-29 15:08:09 -0700

    Removed Comment Seed

--- src/db/seeds/02-comments.js
+++
@@ -1,9 +0,0 @@
-const comments = require("./02-comments.json");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE comments RESTART IDENTITY CASCADE")
-    .then(() => {
-      return knex("comments").insert(comments);
-    });
-};
